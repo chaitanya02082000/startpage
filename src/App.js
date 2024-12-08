91ef85a3f9590fbc95329e1a96fc64b0bdb0a45a
Chaitanya <112265682+chaitanya02082000@users.noreply.github.com>
2024-12-08 16:53:14 +0000

    this the base

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <p>link1</p>
+    <p>link 2</p>
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    </div>
+  );
+}
+
+export default App;
